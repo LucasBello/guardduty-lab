a46c45b74aca19ecaf5b96634bba6e8ed7a7d459
Lucas <lucas.bello@yahoo.com.br>
2021-11-01 15:34:46 -0300

    Update bastion_bootstrap.sh

--- bastion_bootstrap.sh
+++ bastion_bootstrap.sh
@@ -1,23 +1,3 @@
-# Copyright 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-#  
-#  Licensed under the Apache License, Version 2.0 (the "License").
-#  You may not use this file except in compliance with the License.
-#  A copy of the License is located at
-#  
-#      http://www.apache.org/licenses/LICENSE-2.0
-#  
-#  or in the "license" file accompanying this file. This file is distributed 
-#  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
-#  express or implied. See the License for the specific language governing 
-#  permissions and limitations under the License.
-
-#!/bin/bash -e
-# Bastion Bootstrapping
-# authors: tonynv@amazon.com, sancard@amazon.com, ianhill@amazon.com
-# NOTE: This requires GNU getopt. On Mac OS X and FreeBSD you must install GNU getopt and mod the checkos function so that it's supported
-
-
-# Configuration
 PROGRAM='Linux Bastion'
 
 ##################################### Functions Definitions
@@ -27,7 +7,7 @@ function checkos () {
     if [[ "$unamestr" == 'Linux' ]]; then
         platform='linux'
     else
-        echo "[WARNING] This script is not supported on MacOS or freebsd"
+        echo "[AVISO] Este script não tem suporte para FreeBSD"
         exit 1
     fi
     echo "${FUNCNAME[0]} Ended"
